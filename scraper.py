4bcc087e2fc3b5e6d36ab7fdc94e5b9ad097318f
APP <magdolinemyby@gmail.com>
2026-04-30 00:17:14 +0100

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,8 @@
+   {
+     "type": "service_account",
+     "project_id": "myby-scraper",
+     "private_key_id": "abc123...",
+     "private_key": "-----BEGIN PRIVATE KEY-----\nMIIE...\n-----END PRIVATE KEY-----\n",
+     "client_email": "myby-scraper-bot@myby-scraper.iam.gserviceaccount.com",
+     ...
+   }
